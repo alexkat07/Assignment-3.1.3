770ca86fb873c93c9a1b2fe13484d6449f496ee5
alexk <alexk@DESKTOP-QL7PNT4>
2024-05-20 15:24:58 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment_3._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a string");
+            string userInput = Console.ReadLine();
+            Console.WriteLine($"\"{userInput}\" contains {NumberOfSpaces(userInput)} spaces");
+        }
+
+        static int NumberOfSpaces(string userInput) 
+        {
+            int spaces = 0;
+            for(int i = 0; i < userInput.Length; i++)
+            {
+                if (userInput[i] == ' ') 
+                {
+                    spaces++;
+                }
+            }
+            return spaces;
+        }
+    }
+}
